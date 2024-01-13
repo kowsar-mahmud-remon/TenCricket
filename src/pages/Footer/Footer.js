38148c123c9a3fd5ed6d33a5cd0169b7c39db37a
creativerze <creativerze@gmail.com>
2024-01-13 17:23:55 +0600

    added footer

---
+++ src/pages/Footer/Footer.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { FaFacebookSquare, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="bg-secondary text-white">
+      <div className="max-w-[1280px] mx-auto">
+        <footer className="footer px-5 py-10 ">
+          <div className="">
+            <div className="flex items-center">
+
+              <p className="text-2xl font-semibold">Ten Cricket</p>
+            </div>
+            <div className="mt-2">
+              <p>Need any Help</p>
+              <p className="mt-1 text-lg font-semibold">
+                Call Us: <span className="text-[#fcbf49]"> +880 12354135</span>
+              </p>
+              <p className="mt-4 mb-1">Love street, Muscat, Oman</p>
+              <p>example@gmail.com</p>
+            </div>
+
+            <div className="flex gap-4 text-lg mt-4">
+              <FaFacebookSquare className="cursor-pointer" />
+              <FaTwitter className="cursor-pointer" />
+              <FaLinkedin className="cursor-pointer" />
+            </div>
+          </div>
+          <div className="footer">
+            <div>
+              <span className="footer-title">Company</span>
+              <Link to="/" className="link link-hover">
+                About Us
+              </Link>
+              <Link to="/" className="link link-hover">
+                Contact Us
+              </Link>
+              <Link to="/" className="link link-hover">
+                Travel Guides
+              </Link>
+              <Link to="/" className="link link-hover">
+                Data Policy
+              </Link>
+            </div>
+            <div>
+              <span className="footer-title">Top Shop</span>
+              <Link to="/" className="link link-hover">
+                Las Vegas
+              </Link>
+              <Link to="/" className="link link-hover">
+                New York City
+              </Link>
+              <Link to="/" className="link link-hover">
+                San Francisco
+              </Link>
+              <Link to="/" className="link link-hover">
+                Hawaii
+              </Link>
+            </div>
+            <div>
+              <span className="footer-title mb-7"></span>
+              <Link to="/" className="link link-hover">
+                Tokyo
+              </Link>
+              <Link to="/" className="link link-hover">
+                Sydney
+              </Link>
+              <Link to="/" className="link link-hover">
+                Melbourne
+              </Link>
+              <Link to="/" className="link link-hover">
+                Dubai
+              </Link>
+            </div>
+          </div>
+          <div className="">
+            <p className="text-md font-semibold">Sign up Newsletter</p>
+            <input
+              className="py-2 px-4 rounded mt-2"
+              type="text"
+              name=""
+              id=""
+              placeholder="Enter email"
+            />
+            <button className="rounded bg-[#fcbf49] py-2 px-6 ">Submit</button>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
